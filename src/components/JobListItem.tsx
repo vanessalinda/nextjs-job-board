8795aeda90b78386cd9dd8e60850f1075d83be7d
Vanessa Power <vanessa.l.power@gmail.com>
2024-01-17 15:08:12 -0800

    initiate prisma client and create schema

---
+++ src/components/JobListItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Job } from "@prisma/client";
+
+interface JobListItemProps {
+  job: Job;
+}
+
+const JobListItem = () => {
+  return <div>JobListItem</div>;
+};
+
+export default JobListItem;
